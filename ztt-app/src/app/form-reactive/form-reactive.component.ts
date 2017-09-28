ae59bd18f9b57022524075a12318aed074bef9cc
Your Nameztiantian <243752773@qq.com>
2017-09-28 15:57:14 +0800

    form

--- ztt-app/src/app/form-reactive/form-reactive.component.ts
+++ ztt-app/src/app/form-reactive/form-reactive.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup,FormBuilder} from '@angular/forms';
+import {FormControl,FormGroup,FormBuilder,Validators} from '@angular/forms';
 @Component({
   selector: 'app-form-reactive',
   templateUrl: './form-reactive.component.html',
   styleUrls: ['./form-reactive.component.css']
 })
 export class FormReactiveComponent implements OnInit {
+  mobileValidator(control:FormControl):any{
+    let mbreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
+    let valid = mbreg.test(control.value)
+    return valid ? null:{mobile:true}
+  }
   formModel:FormGroup;
   constructor(fb:FormBuilder) {
     this.formModel = fb.group({
-      usename:[''],
-      password:['']
+      usename:['',[Validators.required]],
+      password:[''],
+      mobile:['',this.mobileValidator]
     })
    }
 
   ngOnInit() {
   }
-  clickSubmit(){
-    console.log(this.formModel.value)
+  onSubmit(){
+    let isValid:boolean = this.formModel.get('usename').valid;
+    console.log(this.formModel.value,isValid)
   }
 }
